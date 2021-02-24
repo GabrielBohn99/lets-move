b79072a541d69174033c722082adabb8ad4fa5e1
Gabriel Bohn <gabrielcbohn@gmail.com>
2021-02-24 13:45:28 -0300

    Third day done

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompleteChallenge () {
+    const { challengesComplited } = useContext(ChallengesContext)
     return (
         <div className={styles.completedChallengesContainer}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{challengesComplited}</span>
         </div>
     )
 }
